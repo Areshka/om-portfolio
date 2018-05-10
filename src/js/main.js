5837798936cdc917207c5e9fd046289d18951985
Oleg <oleg1990moskalenko@gmail.com>
2018-05-10 14:03:35 +0300

    added scrollspy.js and smooth transition to the anchor

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@
 // Импортируем необходимые js-файлы Bootstrap 4
 //= ../bower_components/bootstrap4/js/dist/util.js
 //= ../bower_components/bootstrap4/js/dist/collapse.js
-
+//= ../bower_components/bootstrap4/js/dist/scrollspy.js
 
 /* ------------------------------------------------------------------
   main-nav open-close
@@ -23,7 +23,36 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove("mainNavigation--opened");
   }
 });
-
 /* ------------------------------------------------------------------
   end main-nav open-close
+---------------------------------------------------------------------*/
+
+/* ------------------------------------------------------------------
+  begin плавный переход к якорю
+---------------------------------------------------------------------*/
+$(document).ready(function(){
+	$("#navbarSupportedContent").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+
+	$(".mainHeader").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
+/* ------------------------------------------------------------------
+  end плавный переход к якорю
 ---------------------------------------------------------------------*/
